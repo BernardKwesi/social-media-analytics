2dc3ca0a5d4b89358851424d61d4808bdbb191f7
BernardKwesi <bernarddormon31@gmail.com>
2026-01-20 12:04:33 +0000

    Update

--- src/app/context/auth-context.tsx
+++ src/app/context/auth-context.tsx
@@ -405,8 +405,7 @@ export function AuthProvider({ children }: { children: ReactNode }) {
       const { error } = await supabase.auth.signInWithOAuth({
         provider: "google",
         options: {
-          // redirectTo: `${window.location.origin}/`,
-          redirectTo: `http://localhost:5174/`,
+          redirectTo: `${window.location.origin}/`,
           skipBrowserRedirect: false,
         },
       });
